206cf5f1be2eab359341a4d6df31e859b98aaf2f
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-31 08:33:59 +0200

    test Unitaire

---
+++ src/main/java/com/sip/ams/Amsdata20233008Application.java
@@ -0,0 +1,21 @@
+package com.sip.ams;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import com.sip.ams.controllers.ArticleController;
+
+import java.io.File;
+
+@SpringBootApplication
+public class Amsdata20233008Application {
+
+	public static void main(String[] args) {
+		new File(ArticleController.uploadDirectory).mkdir();
+		SpringApplication.run(Amsdata20233008Application.class, args);
+	}
+
+}
